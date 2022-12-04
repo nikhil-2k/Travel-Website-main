cc3dc1a33116e68e72794fa7d260fa92b150d203
nikhil_21 <nikhilthakur2k@gmail.com>
2022-12-04 20:10:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Travel-Website
+
+- This project uses basic HTML, CSS and JavaScript
+
+- Features
+  * It consists of home page
+  * Built in Gallery
+  * Working Shopping cart
+  * Description page
+  
+- Clone this project using VScode
+- Run and use it for your college project.
---
+++ safaraahi/shopping cart/products.js
@@ -0,0 +1,94 @@
+const products = [
+  {
+    id: 0,
+    name: "Goa",
+    price: 4999,
+    instock: 100,
+    description:
+      "The state of Goa, in India, is famous for its beaches and places of worship. Tourism is its primary industry, and is generally focused on the coastal areas of Goa, with decreased tourist activity inland.",
+    location: "Goa",
+    imgSrc: "./img/goa.png",
+  },
+  {
+    id: 1,
+    name: "Golden Temple",
+    price: 3999,
+    instock: 43,
+    description:
+      "Also known as Sri Harmandir Sahib, this beautiful religious place of the Sikh community is all about tranquillity, humanity and brotherhood.",
+    location: "Golden Temple Rd, Atta Mandi, Katra Ahluwalia, Amritsar, Punjab",
+    imgSrc: "./img/golden_temple.jpg",
+  },
+  {
+    id: 2,
+    name: "India Gate",
+    price: 6999,
+    instock: 10,
+    description:
+      "This iconic sandstone-granite arch gate is among the largest war memorials in the country. The beauty of this important monument is accentuated by the verdant lawns that surround it.",
+    location: "Rajpath, India Gate, New Delhi",
+    imgSrc: "./img/india_gate.jpg",
+  },
+  {
+    id: 3,
+    name: "Lotus Temple",
+    price: 5999,
+    instock: 5,
+    description:
+      "Lotus Temple, also known as Baha’I Lotus Temple or Kamal Mandir, is an architectural marvel shaped like a white lotus flower.",
+    location: "Lotus Temple Road, Bahapur, Kalkaji, New Delhi",
+    imgSrc: "./img/lotus_temple.jpg",
+  },
+  {
+    id: 4,
+    name: "Qutub Minar",
+    price: 4999,
+    instock: 4,
+    description:
+      "Qutub Minar, also known as victory tower, is one of the tallest monuments in Asia and the tallest brick minaret in the world, at 72.5m.",
+    location: "Seth Sarai, Mehrauli, New Delhi" ,
+    imgSrc: "./img/qutub_minar.jpg",
+  },
+  {
+    id: 5,
+    name: "Red Fort",
+    price: 3999,
+    instock: 40,
+    description:
+      "Constructed during the reign of Shah Jahan in the 17thcentury, Red Fort or Lal Qila is one of the most popular historical buildings of India.",
+    location: "Netaji Subhash Marg, Chandni Chowk, New Delhi",
+    imgSrc: "./img/red_fort.jpg",
+  },
+
+  {
+    id: 6,
+    name: "Sun Temple",
+    price: 2999,
+    instock: 40,
+    description:
+      "Dedicated to the Sun God, this temple stands out for its Oriyan form of architecture. Konark Sun Temple was built in the 13th century and is shaped like a massive chariot with stone wheels, walls and pillars.",
+    location: "Konark, Puri district, Odisha",
+    imgSrc: "./img/sun_temple.jpg",
+  },
+  {
+    id: 7,
+    name: "Taj Mahal",
+    price: 7999,
+    instock: 40,
+    description:
+      "Built by the Mughal Emperor Shah Jahan, this intricately carved 17th-century white marble mausoleum grace the southern bank of the Yamuna River and is one of the Seven Wonders of the World. ",
+    location: "Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh",
+    imgSrc: "./img/tajmahal.jpg",
+  },
+  {
+    id: 8,
+    name: "Manali",
+    price: 9999,
+    instock: 40,
+    description:
+      "A gift of the Himalayas to the world, Manali is a beautiful township nestled in the picturesque Beas River valley. It is a rustic enclave known for its cool climate and snow-capped mountains, offering respite to tourists escaping scorching heat of the plains.",
+    location: "Manali, Kullu, Himachal Pradesh",
+    imgSrc: "./img/uttarakhand.jpg",
+  },
+ 
+];
